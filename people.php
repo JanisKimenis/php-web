5a840e78fa98fc25609c035c29c57a197b0215c4
you <you@gmail.com>
2025-09-09 11:41:49 +0300

    Versionaaaa php

--- people.php
+++ people.php
@@ -1,4 +1,40 @@
 <?php
+class Person {
+    public $name;
+    public $email;
+    public $progLangs = [];
+
+    function set_name($name){
+        $this->name = $name;
+    }
+    function set_email($email){
+        $this->email = $email;
+    }
+    function set_progLangs($progLangs){
+        $this->progLangs = $progLangs;
+    }
+    function get_name(){
+        return $this->name;
+    }
+    function get_email(){
+        return $this->email;
+    }
+    function get_progLangs(){
+        return $this->progLangs;
+    }
+    function __constructor($name, $email, $progLangs){
+        $this->name = $name;
+        $this->email = $email;
+        $this->progLangs = $progLangs;
+    }
+    function printLangs(array $langs) {
+        foreach($langs as $item) {
+        echo $item;
+    }
+}
+}
+
+$person1 = new Person("John", "Pork@gmail.com", array("C#", "C++", "php"));
 $info1 = array("name"=>"John", "email"=>"Pork@gmail.com", "programming-languages"=>array("C#", "C++", "php"));
 $info2 = array("name"=>"Mike", "email"=>"Hawk@gmail.com", "programming-languages"=>array("Rust"));
 $info3 = array("name"=>"Opium", "email"=>"User@gmail.com", "programming-languages"=>array("C#", "C++", "php", "Rust", "lua", "C", "js", "blockprogramming", "Ruby", "Java", "python", "mySQL", "R", "Luau"));
@@ -12,9 +48,9 @@ $info3 = array("name"=>"Opium", "email"=>"User@gmail.com", "programming-language
 </head>
 <body>
     <ol>
-        <li>Name: <?php echo $info1["name"];?></li>
-        <li>Email: <?php echo $info1["email"]?></li>
-        <li>Programming languages:  <?php foreach($info1["programming-languages"] as $language){echo "$language  ";} ?></li>
+        <li>Name: <?php echo $person1->get_name();?></li>
+        <li>Email: <?php echo $person1->get_email();?></li>
+        <li>Programming languages:  <?php printLangs($person1->get_progLangs()) ?></li>
     </ol>
     <ol>
         <li>Name: <?php echo $info2["name"];?></li>
